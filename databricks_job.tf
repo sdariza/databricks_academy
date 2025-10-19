0cd161bc508323747eb3ef179f21ca8a60ee3398
sdariza <sdariza@uninorte.edu.co>
2025-10-18 22:15:56 -0500

    Add first notebook job configuration with scheduling and parameters

--- databricks_job.tf
+++ databricks_job.tf
@@ -1,9 +1,18 @@
 resource "databricks_job" "notebook_job" {
-  name = "Example Notebook Job"
+  name = "My first notebook job with terraform"
   schedule {
     quartz_cron_expression = "0 0 2 * * ?"
     timezone_id            = "America/Bogota"
     pause_status           = "UNPAUSED"
+  }
+  task {
+    task_key = "hello_world"
+    notebook_task {
+      notebook_path = ""
+      base_parameters = {
+        msg = "Hello world, this is my first lakeflow job with terraform!"
+      }
+    }
   }
   max_concurrent_runs = 1
   tags = {
