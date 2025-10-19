dde3f956211f5d18e8b1e34ea796365324c09293
sdariza <sdariza@uninorte.edu.co>
2025-10-18 22:31:03 -0500

    Fix notebook path in databricks job configuration

--- databricks_job.tf
+++ databricks_job.tf
@@ -8,7 +8,7 @@ resource "databricks_job" "notebook_job" {
   task {
     task_key = "hello_world"
     notebook_task {
-      notebook_path = "/"
+      notebook_path = "/Workspace/Users/sdariza.certifications@gmail.com/TEST/hello_world"
       base_parameters = {
         msg = "Hello world, this is my first lakeflow job with terraform!"
       }
