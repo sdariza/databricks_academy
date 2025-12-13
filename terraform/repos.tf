60f402810af43951a19ee5eee14fdf4e34276479
sdariza <sdariza@uninorte.edu.co>
2025-12-13 11:46:34 -0500

    fix: 🐛 update branch in Databricks repo configuration from 'dev' to 'main'

--- terraform/repos.tf
+++ terraform/repos.tf
@@ -1,6 +1,6 @@
 resource "databricks_repo" "git_repo" {
   url    = "https://github.com/sdariza/databricks_academy.git"
-  branch = "dev"
+  branch = "main"
 }
 
 resource "databricks_git_credential" "git_credential" {
